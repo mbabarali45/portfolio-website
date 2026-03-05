48b7f3673fc577177cd01b84fae268deaed4ec9a
root <root@ip-172-31-31-92.eu-north-1.compute.internal>
2026-03-05 14:43:14 +0000

    portfolio website

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage(){
+
+alert("Welcome to Babar Portfolio Website");
+
+}
+
+
+function darkMode(){
+
+document.body.classList.toggle("dark");
+
+}
+
+
+console.log("Portfolio Website Loaded");
